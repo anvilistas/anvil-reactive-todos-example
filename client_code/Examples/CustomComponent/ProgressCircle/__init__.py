16422dada7786878e2c4a136ece923cb08ae8c44
stucork@gmail.com <stucork@gmail.com>
2023-12-13 15:31:20 +0000

    Edited Examples.CustomComponent.ProgressCircle and settings

--- client_code/Examples/CustomComponent/ProgressCircle/__init__.py
+++ client_code/Examples/CustomComponent/ProgressCircle/__init__.py
@@ -2,14 +2,16 @@ from ._anvil_designer import ProgressCircleTemplate
 from anvil_reactive.main import render_effect, signal, computed
 import math
 
+
+
 class ProgressCircle(ProgressCircleTemplate):
     """Inspired by: https://anvil.works/forum/t/progressbar-component-again/18656"""
     value = signal(0)
     color = signal("")
+    circ = 2 * math.pi * 40
 
     def __init__(self, **properties):
         self.init_components(**properties)
-        self.circ = circ = 2 * math.pi * float(self.dom_nodes["circle"].getAttribute("r"))
 
     @render_effect
     def render_color(self):
