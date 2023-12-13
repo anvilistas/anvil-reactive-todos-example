5a689c98304bb39528f46c8f52462818ec580d35
stucork@gmail.com <stucork@gmail.com>
2023-12-13 13:12:34 +0000

    Edited Examples.CustomComponent.ProgressCircle and Examples.CustomComponent

--- client_code/Examples/CustomComponent/ProgressCircle/__init__.py
+++ client_code/Examples/CustomComponent/ProgressCircle/__init__.py
@@ -3,18 +3,18 @@ from anvil_reactive.main import render_effect, signal
 import math
 
 class ProgressCircle(ProgressCircleTemplate):
+    """Inspired by: https://anvil.works/forum/t/progressbar-component-again/18656"""
     value = signal(0)
 
     def __init__(self, **properties):
         self.init_components(**properties)
+        self.circ = circ = 2 * math.pi * float(self.dom_nodes["circle"].getAttribute("r"))
 
     @render_effect
     def render(self):
         progress = self.value or 0
-        if progress >= 100:
+        if progress > 100:
             return
-
-        circ = 2 * math.pi * 40
-        offset = circ * (1 - progress / 100);
+        offset = self.circ * (1 - progress / 100);
         self.dom_nodes["circle"].setAttribute("stroke-dashoffset", offset)
         self.dom_nodes["text"].textContent = f"{progress:.0f}%"
