3066689149229dd05e0382460be2872c06dd634a
stucork@gmail.com <stucork@gmail.com>
2024-10-18 02:08:09 +0000

    Edited Examples.TodosExample.TodosStore

--- client_code/Examples/TodosExample/TodosStore.py
+++ client_code/Examples/TodosExample/TodosStore.py
@@ -4,6 +4,16 @@ from uuid import uuid4
 
 todos_db = indexed_db
 
+from anvil_reactive.main import reactive_list
+
+def sort(self, *args, **kws):
+    debugger
+    rv = list.sort(self, *args, **kws)
+    self.LIST_LEN.update()
+    return rv
+
+reactive_list.sort = sort
+
 @reactive_class
 class TodoStore:
     def __init__(self):
