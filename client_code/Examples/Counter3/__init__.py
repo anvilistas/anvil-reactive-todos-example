2cb186e13cd45604b72f0a8abaa5114d32cbbfde
stucork@gmail.com <stucork@gmail.com>
2024-10-23 15:33:39 +0000

    Edited Examples.Counter3

--- client_code/Examples/Counter3/__init__.py
+++ client_code/Examples/Counter3/__init__.py
@@ -6,13 +6,15 @@ class Counter3(Counter3Template):
     def __init__(self, **properties):
         self.counter = 0
         self.title_label.text = type(self).__name__
-        writeback(self, "foo", self, "counter", "x-change")
+        self.foo = self.counter
+        writeback(self, "counter", self, "foo", "x-change")
         
         self.init_components(**properties)
 
     @render_effect
     def render(self):
         self.label_1.text = self.counter
+
 
     def btn_plus_click(self, **event_args):
         self.counter += 1
@@ -21,4 +23,9 @@ class Counter3(Counter3Template):
     def btn_minus_click(self, **event_args):
         self.counter -= 1
         self.raise_event("x-change")
+
+    def button_1_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.foo *= 2
+        # self.raise_event("x-change")
 
