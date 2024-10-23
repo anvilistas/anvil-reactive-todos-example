c8e7205ac2277c451e801e9ae7d1b79ac4da6606
stucork@gmail.com <stucork@gmail.com>
2024-10-23 14:36:31 +0000

    Edited Examples.Counter3 and settings

--- client_code/Examples/Counter3/__init__.py
+++ client_code/Examples/Counter3/__init__.py
@@ -1,11 +1,13 @@
 from ._anvil_designer import Counter3Template
-from anvil_reactive.main import signal, render_effect, effect, bind, reactive_class
+from anvil_reactive.main import signal, render_effect, effect, bind, reactive_class, writeback
 
 @reactive_class
 class Counter3(Counter3Template):
     def __init__(self, **properties):
         self.counter = 0
         self.title_label.text = type(self).__name__
+        # writeback(self, "foo", self, "counter", "x-change")
+        
         self.init_components(**properties)
 
     @render_effect
@@ -14,6 +16,9 @@ class Counter3(Counter3Template):
 
     def btn_plus_click(self, **event_args):
         self.counter += 1
+        self.raise_event("x-change")
 
     def btn_minus_click(self, **event_args):
         self.counter -= 1
+        self.raise_event("x-change")
+
